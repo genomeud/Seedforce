f8c84f1d85d2d8d610b32e37434cdd00af15ec32
PalomaPerez <62282961+palperbel@users.noreply.github.com>
2025-12-16 14:19:09 +0100

    a12_gene_flow_Nm.r

---
+++ dracocephalum_austriacum/functions/a12_gene_flow_Nm.r
@@ -0,0 +1,99 @@
+# Copyright:	    Paloma Perez & Fabio Marroni 2024
+# Aim:              Heatmap plot with FST from stacks
+# To add:		     
+# Suggestions: 
+# Fixes:  
+
+# Run with --help or -h flag for help.
+
+suppressPackageStartupMessages({
+  library(optparse)
+})
+
+option_list = list(
+  make_option(c("-I", "--infile"), type="character",
+  default=NULL,help="square matrix with Nm values for each pop-pop comparison [default= %default]", metavar="character"),
+  make_option(c("-O", "--outpath"), type="character",
+  default=NULL,help="Output directory [default= %default]", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+  
+if (is.null(opt$infile)) {
+  stop("WARNING: No input file specified with '-I' flag.")
+} else {  cat ("input file ", opt$infile, "\n")
+  infile <- opt$infile  
+  }
+  
+if (is.null(opt$outpath)) {
+  stop("WARNING: No outpath specified with '-O' flag.")
+} else {  cat ("outpath ", opt$outpath, "\n")
+  outpath <- opt$outpath  
+  }
+
+# --- #
+# FST # STACKS
+# --- #
+
+fst <- read.table(infile, row.names=1, header=T, check.names = FALSE)
+# we need to change triangular matrix into square matrix
+tri.to.squ<-function(x)
+{
+rn<-row.names(x)
+cn<-colnames(x)
+an<-unique(c(cn,rn))
+myval<-x[!is.na(x)]
+mymat<-matrix(1,nrow=length(an),ncol=length(an),dimnames=list(an,an))
+for(ext in 1:length(cn))
+{
+ for(int in 1:length(rn))
+ {
+ if(is.na(x[row.names(x)==rn[int],colnames(x)==cn[ext]])) next
+ mymat[row.names(mymat)==rn[int],colnames(mymat)==cn[ext]]<-x[row.names(x)==rn[int],colnames(x)==cn[ext]]
+ mymat[row.names(mymat)==cn[ext],colnames(mymat)==rn[int]]<-x[row.names(x)==rn[int],colnames(x)==cn[ext]]
+ }
+  
+}
+return(mymat)
+}
+
+newfst=tri.to.squ(fst)
+
+# plot heatmap
+library(RColorBrewer)
+library(gplots)
+fileOut<-file.path(outpath,"heatmap_Nm_pops_dracocephalum.png")
+png(fileOut,width=16,height=15,units="cm",res=300, type="cairo")
+par(cex.axis = 0.8, cex.lab = 1)
+par(cex.main = 1.1)
+par(mar=c(0,0,1,1), mgp=c(0.5,0.5,0),tck=-0.03,oma=c(1,0.1,1,0.5))
+min_val <- min(newfst)
+max_val <- max(newfst)
+intermediate_val <- (min_val + max_val) / 2
+heatmap(
+  as.matrix(newfst),
+  scale = "none",
+  xlab = "",
+  ylab = "",
+  main=c(expression(paste(italic("D. austriacum")))),
+  col = colorRampPalette(brewer.pal(8, "Blues"))(25),
+  Rowv = TRUE,       # keep row clustering
+  Colv = "Rowv",     # force columns to match row order
+  dendrogram = "row" # only show dendrogram on rows
+)
+# legend(x="topright",title="FST", legend=c(round(min_val, 2), round(intermediate_val, 2), round(max_val, 2)), fill=colorRampPalette(brewer.pal(8, "Oranges"))(3))
+legend(
+  x = "topright",
+  title = "Nm",
+  legend = c(round(min_val, 2), round(intermediate_val, 2), round(max_val, 2)),
+  fill = colorRampPalette(brewer.pal(8, "Blues"))(3)
+)
+pos=2
+cex_text=1.7
+mtext(paste0(toupper(LETTERS[pos]), ")"), 2, line=-9.1, at=0, padj=-25.8, adj=1, cex=cex_text, las=2, font=2)
+dev.off()
+
+# Rowv=FALSE
+# Colv=FALSE
